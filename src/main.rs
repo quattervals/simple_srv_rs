e71abbb9c3b9cd53eefb5be5cf5e3807d503339f
Oliver With <oliverwith@bbv.ch>
2025-07-07 13:41:09 +0200

    async server

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,31 @@
-use std::{
-    fs,
-    io::{BufReader, prelude::*},
-    net::{TcpListener, TcpStream},
-};
+use async_std::{net::TcpListener, net::TcpStream, prelude::*, task};
+use futures::StreamExt;
+use std::{fs, time::Duration};
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+#[async_std::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").await.unwrap();
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        handle_connection(stream);
-    }
+    listener
+        .incoming()
+        .for_each_concurrent(None, |tcp_stream| async move {
+            let tcp_stream = tcp_stream.unwrap();
+            task::spawn(handle_connection(tcp_stream));
+        })
+        .await;
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&stream);
+async fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).await.unwrap();
 
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let get = b"GET / HTTP/1.1";
+    let sleep_request = b"GET /sleep HTTP/1.1";
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep_request) {
+        task::sleep(Duration::from_secs(5)).await;
         ("HTTP/1.1 200 OK", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
@@ -27,9 +34,9 @@ fn handle_connection(mut stream: TcpStream) {
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
-    let response = format!(
-        "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-    );
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    stream.write_all(response.as_bytes()).unwrap();
+    stream.write_all(response.as_bytes()).await.unwrap();
+
+    stream.flush().await.unwrap();
 }
